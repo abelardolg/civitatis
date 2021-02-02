fb30f1e0fab0ac4bf6fd6677473e28deb8649c49
abelardo <abelardolg@gmail.com>
2021-02-02 18:05:07 +0100

    Added Booking Repository Added Booking migration Modified Booking entity - private id Added Booking mapping configuration lines

--- src/CivitatisSoftware/Activity/Infrastructure/ActivityRepository.php
+++ src/CivitatisSoftware/Activity/Infrastructure/ActivityRepository.php
@@ -1,10 +1,11 @@
 <?php
 
 
-namespace App\Tests\CivitatisSoftware\Activity\Infrastructure;
+namespace App\CivitatisSoftware\Activity\Infrastructure;
 
 use App\CivitatisSoftware\Activity\Domain\Activity;
 use App\Tests\CivitatisSoftware\Activity\Domain\BaseRepository;
+use DateTimeImmutable;
 use Doctrine\Common\Collections\Criteria;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMException;
@@ -16,15 +17,21 @@ class ActivityRepository extends BaseRepository
         return Activity::class;
     }
 
-    public function findActivitiesInThisDate(): Criteria
+    public static function createFindActivitiesInThisDateCriteria(DateTimeImmutable $activityDate): Criteria
     {
         return Criteria::create()
-            ->andWhere(Criteria::expr()->gte('availabilityStartDate', true))
-            ->andWhere(Criteria::expr()->lte('availabilityEndDate', true))
+            ->andWhere(Criteria::expr()->gte('availabilityStartDate', $activityDate))
+            ->andWhere(Criteria::expr()->lte('availabilityEndDate', $activityDate))
             ->orderBy(['popularity' => 'DESC']);
     }
 
+    public function findActivitiesInThisDate(DateTimeImmutable $activityDate): Criteria
+    {
+        return ActivityRepository::createFindActivitiesInThisDateCriteria($activityDate);
+    }
+
     /**
+     * @param Activity $activity
      * @throws ORMException
      * @throws OptimisticLockException
      */
