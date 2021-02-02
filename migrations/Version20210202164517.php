debbed6de909fefd58983188ac21584ddf8545d1
abelardo <abelardolg@gmail.com>
2021-02-02 17:55:16 +0100

    Added Booking Repository Added Booking migration Modified Booking entity - private id Added Booking mapping configuration lines

---
+++ migrations/Version20210202164517.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210202164517 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Create an `Booking` table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql("
+            CREATE TABLE booking (
+                id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                activity_id INT NOT NULL,
+                numPax INT NOT NULL DEFAULT 0,
+                price FLOAT NOT NULL DEFAULT 0.0,
+                bookDate DATETIME NOT NULL,
+                doneDate DATETIME NOT NULL,
+                created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+                INDEX IDX_bookDate (bookDate),
+                INDEX IDX_doneDate (doneDate),
+                FOREIGN KEY (activity_id)
+                    REFERENCES  activity(id)
+                    ON UPDATE CASCADE ON DELETE RESTRICT
+            )
+        ");
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql("DROP TABLE 'booking'");
+
+    }
+}
